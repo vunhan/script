7989a0b3f957866fb83adec70b7a50a22455b2bc
bol0bal4 <vunhan93@gmail.com>
2015-04-08 03:03:08 +0700

    Thêm script android notif" soft lnk to /usr/share/android-notifier-desktop/run.sh

--- ap.sh
+++ ap.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-create_ap --no-virt wlp6s0 enp5s0 ArchLinux minkinieLT3
-
+# create_ap --no-virt wlp6s0 enp5s0 ArchLinux bol0bal47198 
+create_ap --no-virt wlp6s0 ppp0 ArchLinux bol0bal47198
+# create_ap --no-virt wlp6s0 wlp6s0 ArchLinux bol0bal47198
+# create_ap --no-virt wlo1 ppp0 ArchLinux bol0bal47198
