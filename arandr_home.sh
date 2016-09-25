b50a896c46ea344d6ae72adf39138614baa63b13
bol0bal4 <vunhan93@gmail.com>
2016-09-25 18:18:31 +0700

    something new

---
+++ arandr_home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 0x1024 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1280x1024 --pos 358x0 --rotate normal
