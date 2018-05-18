de29b6badcec7316dff4afca593e2d7e811a640d
vunhan <vunhan93@gmail.com>
2018-05-18 13:24:02 +0700

    create_ap is a script from aur ArchLinux

--- ap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-create_ap --no-virt wlp6s0 enp5s0 ArchLinux bol0bal47198 
-# create_ap --no-virt wlp6s0 ppp0 ArchLinux bol0bal49875
-# create_ap wlp6s0 wlp6s0 ArchLinux bol0bal47198
-# create_ap --no-virt wlo1 ppp0 ArchLinux bol0bal47198
